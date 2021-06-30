181b78667ea9ae85f8c0a388c5539661f2fbbd53
Soren Soe <soren.soe@xilinx.com>
2021-06-30 21:58:14 +0000

    test1

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
+
+static std::string
+get_msg()
+{
+  std::string msg('a',10);
+  return msg;
+}
 
 int main()
 {
-  std::cout << "hello" << std::endl;
+  std::cout << "hello" << '\n';
+  std::cout << "msg" << get_msg() << '\n';
   return 0;
 }
