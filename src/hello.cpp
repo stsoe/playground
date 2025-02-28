0322a184f1c5524bc3a77bd05f5bcdc1cc2e8b0b
Soren Soe <2106410+stsoe@users.noreply.github.com>
2025-02-28 17:57:53 +0000

    induce errors
    Signed-off-by: Soren Soe <2106410+stsoe@users.noreply.github.com>

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include <string>
 
-#include <boost/format.hpp>
+class X
+{
+  std::string m_string;
+public:
+  X(const std::string& str) : m_string(str) {}
+
+  const std::string
+  get_string()
+  {
+    return m_string;
+  }
+};
+
+const std::string hello()
+{
+  std::string str = "hello";
+  X x(str);
+  return x.get_string();
+}
 
 int main()
 {
-  std::cout << "hello" << std::endl;
+  std::cout << hello() << std::endl;
   return 0;
 }
